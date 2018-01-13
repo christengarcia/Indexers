a371ada38b1fbc49168dc2c402ddc0c5cca51db4
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-13 23:17:54 +1100

    Initial commit

---
+++ Indexers/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * C# Program to Implement Use of Indexers
+ */
+
+using System;
+
+namespace Indexers
+{
+    class values
+    {
+        private int[] val = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        public int Length
+        {
+            get { return val.Length; }
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return val[index];
+            }
+            set
+            {
+                val[index] = value;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
